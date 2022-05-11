9f6e599a6b1c99339aed7c57eb09e5fdef384f51
Bobi V <vazov22@gmail.com>
2022-05-11 23:02:19 +0300

    add js (back-to-top)

--- mainjs.js
+++ mainjs.js
@@ -1,8 +1,29 @@
-$(document).ready(function(){
+$(document).ready(function () {
     const clicknav = $('#clicknav');
     const togglenav = $('#togglenav');
 
-    clicknav.click(function(){
+    const backToTop = $('.back-to-top');
+
+    clicknav.click(function () {
         togglenav.slideToggle('slow')
+    });
+
+    $(window).scroll(function () {
+        const getScrollTop = $(window).scrollTop();
+        console.log(getScrollTop);
+        if (getScrollTop > 200) {
+            backToTop.css('display', 'block');
+        }
+        else {
+            backToTop.css('display', 'none');
+        }
+    });
+
+    backToTop.click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        },
+            "slow"
+        )
     })
 });
